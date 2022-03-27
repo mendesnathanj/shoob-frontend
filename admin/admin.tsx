c78146e250128b27c41310c9cb2a4a10c542b4f2
mendesnathanj <mendesnathanj@gmail.com>
2022-03-27 09:32:05 -0700

    setup multiple pages

---
+++ admin/admin.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../src/index.css';
+import Admin from '../src/entries/Admin';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Admin />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
