a6759cf792699b259bef62cd7875daa9d9887f81
mendesnathanj <mendesnathanj@gmail.com>
2022-03-28 09:58:27 -0700

    make Box polymorphic

--- src/components/common/Box/Box.tsx
+++ src/components/common/Box/Box.tsx
@@ -1,11 +1,11 @@
-import { HTMLAttributes } from 'react';
-import styles from './Box.module.scss';
-
-export interface BoxProps extends HTMLAttributes<HTMLDivElement> {
-  padded: 0 | 1 | 2 | 3;
+type BoxOwnProps<E extends React.ElementType> = {
   children: React.ReactNode;
-}
+  as?: E;
+};
 
-export default function Box({ padded, children, ...rest }: BoxProps) {
-  return <div className={` ${styles.box} ${styles[`padded-${padded * 8}`]}`} {...rest}>{children}</div>;
+type BoxProps<E extends React.ElementType> = BoxOwnProps<E> & Omit<React.ComponentProps<E>, keyof BoxOwnProps<E>>;
+
+export default function Box<E extends React.ElementType ='div'>({ children, as }: BoxProps<E>) {
+  const Component = as || 'div';
+  return <Component>{children}</Component>;
 }
