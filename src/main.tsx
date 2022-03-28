831a65ae9b3dc79606fabe2d2132797172713fa7
mendesnathanj <mendesnathanj@gmail.com>
2022-03-27 18:57:32 -0700

    remove default app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Customer from './entries/Customer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Customer />
   </React.StrictMode>,
   document.getElementById('root')
 )
