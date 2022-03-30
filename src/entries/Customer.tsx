8af38b5e57d28c9257a573b2f157a4ae03b564c1
mendesnathanj <mendesnathanj@gmail.com>
2022-03-30 00:17:30 -0700

    add margin to Box

--- src/entries/Customer.tsx
+++ src/entries/Customer.tsx
@@ -2,5 +2,5 @@ import '~themes/customer.scss';
 import Box from '../components/common/Box';
 
 export default function Customer() {
-  return <Box as="p" p={1}>Customer Page</Box>;
+  return <Box as="p" p={1} m={12}>Customer Page</Box>;
 }
