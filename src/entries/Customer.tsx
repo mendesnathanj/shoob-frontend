9e07b978ae68dddda607f34d4654901b48f0f836
mendesnathanj <mendesnathanj@gmail.com>
2022-03-30 23:38:09 -0700

    update story, change padding to rem

--- src/entries/Customer.tsx
+++ src/entries/Customer.tsx
@@ -1,5 +1,5 @@
 import '~themes/customer.scss';
-import Box from '../components/common/Box';
+import Box from '../components/ui/Box';
 
 export default function Customer() {
   return <Box as="p" p={1} m={12}>Customer Page</Box>;
