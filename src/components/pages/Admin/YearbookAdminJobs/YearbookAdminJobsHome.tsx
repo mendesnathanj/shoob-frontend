067a26037e368359ad3cfada6b7c299519ed46c9
mendesnathanj <mendesnathanj@gmail.com>
2022-07-14 17:24:55 -0700

    update and add links

--- src/components/pages/Admin/YearbookAdminJobs/YearbookAdminJobsHome.tsx
+++ src/components/pages/Admin/YearbookAdminJobs/YearbookAdminJobsHome.tsx
@@ -1,3 +1,12 @@
+import routes from '../../../routes';
+import Link from '../../../ui/Link';
+import Page from '../../../ui/Page';
+
 export default function YearbookAdminJobsHome() {
-  return <div />;
+  return (
+    <Page>
+      <Link to={routes.admin.yearbookAdminJobs.new()}>New Yearbook Job</Link>
+      <Link to={routes.admin.yearbookAdminJobs.edit(138)}>Edit 138</Link>
+    </Page>
+  );
 }
