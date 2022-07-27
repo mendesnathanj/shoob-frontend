7880e538807e2cb6596d5d01d297a4c08e69d626
mendesnathanj <mendesnathanj@gmail.com>
2022-07-26 17:25:42 -0700

    move Page

--- src/components/admin/forms/YearbookAdminJobForm/YearbookAdminJobForm.tsx
+++ src/components/admin/forms/YearbookAdminJobForm/YearbookAdminJobForm.tsx
@@ -1,13 +1,10 @@
 import { useQuery } from 'react-query';
-import { Link } from 'react-router-dom';
 // import { merge } from 'lodash';
 import Form from '../../../ui/Form';
-import Page from '../../../ui/Page';
 import Input from '../../../ui/Form/Inputs';
 import Button from '../../../ui/Button';
 import { YearbookAdminJob } from '../../../../models/v2';
 import { ONE_DAY } from '../../../../utils/constants';
-import routes from '../../../routes';
 import YearbookContractDetailFields from './YearbookContractDetailsFields';
 import { SCHEMA } from './utils';
 
@@ -23,48 +20,45 @@ export default function YearbookAdminJobForm({ id }: YearbookAdminJobFormProps) 
   }, { cacheTime: 0, staleTime: ONE_DAY });
 
   return (
-    <Page isLoading={!yearbookAdminJob}>
-      <Link to={routes.admin.root()}>Admin Page</Link>
-      <Form
-        className="grid grid-cols-12 gap-y-5"
-        defaultValues={yearbookAdminJob}
-        onSubmit={async (values) => {
-          console.log(values);
-          // merge(yearbookAdminJob, values);
-          // const res = await yearbookAdminJob?.save({ with: ['yearbookContractDetails'] });
+    <Form
+      className="grid grid-cols-12 gap-y-5"
+      defaultValues={yearbookAdminJob}
+      onSubmit={async (values) => {
+        console.log(values);
+        // merge(yearbookAdminJob, values);
+        // const res = await yearbookAdminJob?.save({ with: ['yearbookContractDetails'] });
 
-          // console.log(res);
-        }}
-        schema={SCHEMA}
+        // console.log(res);
+      }}
+      schema={SCHEMA}
+    >
+      <Form.Section
+        className="col-span-12"
+        collapsible
+        contentClass="grid grid-cols-2 gap-x-8 gap-y-4"
+        title="Contact Information"
       >
-        <Form.Section
-          className="col-span-12"
-          collapsible
-          contentClass="grid grid-cols-2 gap-x-8 gap-y-4"
-          title="Contact Information"
-        >
-          <Input.Select
-            label="School"
-            name="schoolId"
-            options={[
-              { label: 'Enochs', value: 49 },
-              { label: 'Gregory', value: 42 },
-            ]}
-          />
-          <Input label="YB Advisor 1:" name="yearbook_advisor" />
-          <Input label="YB Advisor 1 Email:" name="yearbook_advisor" />
-        </Form.Section>
-        <Form.Section
-          className="col-span-12"
-          collapsible
-          title="Contract Details"
-        >
-          <YearbookContractDetailFields />
-        </Form.Section>
-        <div className="pt-3">
-          <Button submit variant="primary">Submit</Button>
-        </div>
-      </Form>
-    </Page>
+        <Input.Select
+          label="School"
+          name="schoolId"
+          options={[
+            { label: 'Enochs', value: 49 },
+            { label: 'Gregory', value: 42 },
+          ]}
+        />
+        <Input label="YB Advisor 1:" name="yearbook_advisor" />
+        <Input label="YB Advisor 1 Email:" name="yearbook_advisor" />
+      </Form.Section>
+      <Form.Section
+        className="col-span-12"
+        collapsible
+        title="Contract Details"
+      >
+        <YearbookContractDetailFields />
+      </Form.Section>
+      <div className="pt-3">
+        <Button submit variant="primary">Submit</Button>
+      </div>
+    </Form>
   );
 }
--- src/components/pages/Admin/YearbookAdminJobs/YearbookAdminJobsEdit.tsx
+++ src/components/pages/Admin/YearbookAdminJobs/YearbookAdminJobsEdit.tsx
@@ -1,8 +1,16 @@
 import { useParams } from 'react-router';
 import YearbookAdminJobForm from '../../../admin/forms/YearbookAdminJobForm';
+import routes from '../../../routes';
+import Link from '../../../ui/Link';
+import Page from '../../../ui/Page';
 
 export default function YearbookAdminJobEdit() {
   const { id } = useParams();
 
-  return <YearbookAdminJobForm id={id} />;
+  return (
+    <Page maxWidth="xl">
+      <Link className="inline-block mb-4" to={routes.admin.yearbookAdminJobs.show(id as string)}>Back</Link>
+      <YearbookAdminJobForm id={id} />
+    </Page>
+  );
 }
