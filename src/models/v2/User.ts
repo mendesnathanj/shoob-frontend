c1d802d522f7944f9d0c3a00f19e3db551673965
mendesnathanj <mendesnathanj@gmail.com>
2022-08-13 10:29:01 -0700

    update User model

--- src/models/v2/User.ts
+++ src/models/v2/User.ts
@@ -1,9 +1,23 @@
-import { Model } from 'spraypaint';
+import { Attr, BelongsTo, Model } from 'spraypaint';
 import ApplicationRecord from './ApplicationRecord';
+import School from './School';
+
+type Roles = 'ADMIN' |
+  'SCHOOL_ADMIN' |
+  'PRINCIPAL' |
+  'TEACHER' |
+  'SECRETARY' |
+  'DEVELOPER' |
+  'PHOTOGRAPHER' |
+  'EMPLOYEE' |
+  'ROLE_NOT_FOUND';
 
 @Model()
 class User extends ApplicationRecord {
   static jsonapiType = 'users';
+  @Attr() role: Roles;
+  @Attr() schoolId: number;
+  @BelongsTo() school: School;
 }
 
 export default User;
