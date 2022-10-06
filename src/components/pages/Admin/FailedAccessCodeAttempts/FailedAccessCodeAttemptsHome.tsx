5fb8ca0cdb1c0b71778aa619df92612f22984f7e
mendesnathanj <mendesnathanj@gmail.com>
2022-10-06 12:01:25 -0700

    update Header

--- src/components/pages/Admin/FailedAccessCodeAttempts/FailedAccessCodeAttemptsHome.tsx
+++ src/components/pages/Admin/FailedAccessCodeAttempts/FailedAccessCodeAttemptsHome.tsx
@@ -4,7 +4,7 @@ import FailedAccessCodeAttemptsTable from '@/components/admin/forms/FailedAccess
 export default function FailedAccessCodeAttemptsHome() {
   return (
     <Page maxWidth="xl">
-      FailedAccessCodeAttemptsHome Page
+      <h1 className="text-2xl font-display pb-1 mb-5 border-b border-b-gray-200">Failed Access Code Attempts</h1>
       <FailedAccessCodeAttemptsTable />
     </Page>
   );
