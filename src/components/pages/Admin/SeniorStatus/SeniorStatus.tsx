78a32a143f96de3cb49a797548cbf8dbd99969e3
mendesnathanj <mendesnathanj@gmail.com>
2022-10-22 16:57:56 -0700

    update this since adding param would override

--- src/components/pages/Admin/SeniorStatus/SeniorStatus.tsx
+++ src/components/pages/Admin/SeniorStatus/SeniorStatus.tsx
@@ -1,18 +1,29 @@
+import { useMemo } from 'react';
 import Page from '@/components/ui/Page';
 import Link from '@/components/ui/Link';
 import routes from '@/routes';
 import { useAuth } from '@/hooks/useAuth';
 import { User } from '@/models/v2';
 import SeniorTable from '../../../admin/tables/SeniorTable';
-import useQueryString from '@/hooks/useQueryString';
 import ActionSection from './ActionSection';
 import SeniorStatisticsSection from './SeniorStatisticsSection';
+import useRouteQuery from '@/hooks/useRouteQuery';
 
 export default function SeniorStatus() {
   const { user } = useAuth();
-  const queryString = useQueryString();
+  const query = useRouteQuery();
 
-  const schoolId = (queryString.schoolId || (user as User).schoolId) as number;
+  const schoolIdQuery = query.get('schoolId');
+
+  const schoolId = useMemo(() => {
+    if ((user as User).isAdmin()) {
+      if (typeof schoolIdQuery === 'string') return parseInt(schoolIdQuery, 10);
+
+      return (user as User).schoolId;
+    }
+
+    return (user as User).schoolId;
+  }, [schoolIdQuery]);
 
   return (
     <Page className="flex flex-col gap-12" maxWidth="2xl">
